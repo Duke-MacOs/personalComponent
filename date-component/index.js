f3814aeffefd24d33d9badba2d20d272a5af1278
chenzheOS <334925071@qq.com>
2018-01-24 19:52:27 +0800

    finish datarender

--- date-component/index.js
+++ date-component/index.js
@@ -2,9 +2,11 @@
     function DateTimePicker (target) {
         this.init(target);
         // 渲染模板
-        this.render();
+        this.renderHeader();
         // 设置日历数据
         this.setData();
+        // 渲染日历数据
+        this.renderData();
         // this. bind();
         
     }
@@ -15,19 +17,19 @@
             this.target = document.getElementById(target);
         },
         setData: function () {
-            console.log(this.date)
             // 获取当前第一天的Date对象
             function getFirstDay(date) {
-                return new Date(date.getTime() - (date.getDate() - 1)*1000*60*60*24);
+                return new Date(date-0 - (date.getDate() - 1)*1000*60*60*24);
             }
             // 获取当前最后一天的Date对象
             function getLastDay(date) {
-                return new Date(date.getTime() + 1000*60*60*24*(getMonthLength(date) - date.getDate()));
+                return new Date(date-0 + 1000*60*60*24*(getMonthLength(date) - date.getDate()));
             }
             // 计算Date对象当前月有多少天
             function getMonthLength(date) {
                 return new Date(date.getYear(), date.getMonth(), 0).getDate();
             }
+
             var _date = this.date,
                 _year = _date.getYear(),
                 _month = _date.getMonth(),
@@ -35,27 +37,32 @@
                 _lastDay = getLastDay(_date),  // 当前日期最后一天
                 arr = [];
 
+            // 上个月数据放入数组
             for(let i = _firstDay.getDay(), ii = 0; i > ii; i--) {
                 arr.push({type: 'pre-month', date: new Date(_firstDay - 1000*60*60*24*i)});
             }
 
+            // 当前月数据放入数组
             for(let i = 1, ii = getMonthLength(_date), length = arr.length; i <= ii; i++) {
                 if(length) {
                     let last = arr[length - 1].date;
-                    arr.push({type: 'cur-month', date: new Date(last.getTime() + 1000*60*60*24*i)});
+                    arr.push({type: 'cur-month', date: new Date(last-0 + 1000*60*60*24*i)});
                 }else {
-                    arr.push({type: 'cur-month', date: new Date(_firstDay.getTime() + 1000*60*60*24*(i - 1))});
+                    arr.push({type: 'cur-month', date: new Date(_firstDay-0 + 1000*60*60*24*(i - 1))});
                 }
                 
             }
 
+            // 下个月数据放入数组
             for(let i = 1, ii = 6 - _lastDay.getDay(); i <= ii; i++) {
-                arr.push({type: 'next-month', date: new Date(_lastDay.getTime() + 1000*60*60*24*i)})
+                arr.push({type: 'next-month', date: new Date(_lastDay-0 + 1000*60*60*24*i)})
             }
 
+            this.data = [...arr];
         },
-        render: function () {
+        renderHeader: function () {
             this.dataComplate = document.createElement("div");
+            this.dataComplate.setAttribute('id', this.target.id + '_DateTimePicker');
             this.dataComplate.className = "date-time-picker";
             var tpl = `  <div class="header">
                             <span class="pre caret-left"></span>
@@ -69,13 +76,34 @@
                                     <th>日</th><th>一</th><th>二</th><th>三</th><th>四</th><th>五</th><th>六</th>
                                 </tr>
                             </thead>
-                            <tbody>
-                                
-                            </tbody>
                         </table>`;
             this.dataComplate.innerHTML = tpl;
             
             document.getElementsByTagName('body')[0].appendChild(this.dataComplate);
+        },
+        renderData() {
+            var _data = this.data,
+                dateComplate = document.createElement("tbody"), // 创建tbody元素
+                dateTimePickerId = this.target.id + '_DateTimePicker', // 获取datetimepicker HTMLElement对象
+                str = '<tr>';
+
+            // 拼接日期显示字符串
+            for (let val of _data.entries()) {
+                let [index, {type, date}] = val;
+                if (index % 7 === 0 && index !== 0) {
+                    str += '</tr><tr>'+
+                            '<td class="'+ type + '">'+ date.getDate() +'</td>';
+                          
+                }else {
+                    str +=  '<td class="'+ type + '">'+ date.getDate() +'</td>';
+                }
+            }
+            dateComplate.innerHTML = str;
+
+            // 找到当前datetimepicker table元素
+            var table = document.querySelector('#' + dateTimePickerId + ' table');
+            table.appendChild(dateComplate);
+           
         },
         bind: function () {
 
